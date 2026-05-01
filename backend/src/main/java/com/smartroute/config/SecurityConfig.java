3fb37c8efc49a17bbedff944c9cb25049b9232f3
apple <alwsmilemansu23@gmail.com>
2026-05-01 12:55:14 +0530

    swagger implementation

--- backend/src/main/java/com/smartroute/config/SecurityConfig.java
+++ backend/src/main/java/com/smartroute/config/SecurityConfig.java
@@ -1,37 +1,3 @@
-// package com.smartroute.config;
-
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.web.SecurityFilterChain;
-
-// import org.springframework.security.config.Customizer;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.security.crypto.password.PasswordEncoder;
-
-// @Configuration
-// public class SecurityConfig {
-
-//     @Bean
-//     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//         http
-//             .cors(Customizer.withDefaults()) // 👈 THIS IS CRITICAL
-//             .csrf(csrf -> csrf.disable())
-//             .authorizeHttpRequests(auth -> auth
-//                 // .requestMatchers("/api/auth/**").permitAll()
-//                  .requestMatchers("/api/auth/**", "/traffic/**").permitAll()
-//                 .anyRequest().authenticated()
-//             );
-
-//         return http.build();
-//     }
-
-//     @Bean
-//     public PasswordEncoder passwordEncoder() {
-//         return new BCryptPasswordEncoder();
-//     }
-// }
 
 package com.smartroute.config;
 
@@ -59,7 +25,14 @@ public class SecurityConfig {
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/**", "/traffic/**").permitAll()
+                // .requestMatchers("/api/auth/**", "/traffic/**").permitAll()
+                .requestMatchers(
+        "/api/auth/**",
+        "/traffic/**",
+        "/swagger-ui/**",
+        "/v3/api-docs/**",
+        "/swagger-ui.html"
+).permitAll()
                 .anyRequest().authenticated()
             );
 
